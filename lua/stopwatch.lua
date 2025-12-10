2fb9c2bb95ec07829517481ce9b800f9f38ebd82
Edouard D. <zeddydy@hotmail.com>
2025-12-10 13:00:01 +0100

    Add minimum setup for a nvim plugin

--- lua/stopwatch.lua
+++
@@ -1,1 +0,0 @@
-print("Hello Word from stopwatch.nvim")
---
+++ lua/stopwatch/stopwatch.lua
@@ -0,0 +1,9 @@
+---@class stopwatch
+local stopwatch = {}
+
+function stopwatch.setup(opts)
+	opts = opts or {}
+	print("Hello Word from stopwatch.nvim")
+end
+
+return stopwatch
