4ba1db757dcba0cc6b95321fb7477bf086dcc013
Edouard D. <zeddydy@hotmail.com>
2025-12-10 13:11:54 +0100

    Add Hello Word command

--- lua/stopwatch/stopwatch.lua
+++ lua/stopwatch/stopwatch.lua
@@ -3,7 +3,10 @@ local stopwatch = {}
 
 function stopwatch.setup(opts)
 	opts = opts or {}
-	print("Hello Word from stopwatch.nvim")
+
+	vim.api.nvim_create_user_command("Stopwatch", function()
+		print("Hello Word from stopwatch.nvim")
+	end, {})
 end
 
 return stopwatch
